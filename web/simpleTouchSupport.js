a19687a5a459241a1b38917adc0e82cbc956784e
Lasse <LauwerysLasse@gmail.com>
2024-11-13 12:52:17 +0100

    Consistent scaling

--- web/simpleTouchSupport.js
+++ web/simpleTouchSupport.js
@@ -10,17 +10,15 @@ import * as ComfyUI_module from "../../../scripts/app.js"
 /** @type { import("../../../web/scripts/app.js") } */
 const { app } = ComfyUI_module
 
-//////////////////////////////
-
 /**
- * Smooth scrolling for touchpad
+ * Smooth zooming for touchscreen
  */
 let touchZooming;
 let touchCount = 0;
 app.registerExtension({
   name: "Comfy.SimpleTouchSupportFox",
   setup() {
-    let zoomPos
+    let touchDist
     let touchTime
     let lastTouch
     let lastScale
@@ -55,7 +53,7 @@ app.registerExtension({
             lastScale = app.canvas.ds.scale
             lastTouch = getMultiTouchCenter(e)
 
-            zoomPos = getMultiTouchPos(e)
+            touchDist = getMultiTouchPos(e)
             app.canvas.pointer_is_down = false
           }
         }
@@ -72,7 +70,7 @@ app.registerExtension({
           try {
             // hack to get litegraph to use this event
             e.constructor = CustomEvent
-          } catch (error) {}
+          } catch (error) { }
           e.clientX = lastTouch.clientX
           e.clientY = lastTouch.clientY
 
@@ -93,48 +91,38 @@ app.registerExtension({
           // @ts-expect-error
           LiteGraph.closeAllContextMenus()
           app.canvas.search_box?.close()
-          const newZoomPos = getMultiTouchPos(e)
+          const newTouchDist = getMultiTouchPos(e)
 
           const center = getMultiTouchCenter(e)
 
-          let scale = app.canvas.ds.scale
-          const diff = zoomPos - newZoomPos
-
-          scale = lastScale - diff / 100
-
-          const newX = ((center.clientX - lastTouch.clientX) * 1) / scale
-          const newY = ((center.clientY - lastTouch.clientY) * 1) / scale
-
-          const convertCanvasToOffset = (pos, scale) => [
-            pos[0] / scale - app.canvas.ds.offset[0],
-            pos[1] / scale - app.canvas.ds.offset[1]
-          ]
-
-          // Code from LiteGraph
-          if (scale < app.canvas.ds.min_scale) {
-            scale = app.canvas.ds.min_scale
-          } else if (scale > app.canvas.ds.max_scale) {
-            scale = app.canvas.ds.max_scale
-          }
-
+          let scaleDiff = app.canvas.ds.scale
           const oldScale = app.canvas.ds.scale
 
-          app.canvas.ds.scale = scale
+          scaleDiff = lastScale * newTouchDist / touchDist
 
+          const newX = (center.clientX - lastTouch.clientX) / scaleDiff
+          const newY = (center.clientY - lastTouch.clientY) / scaleDiff
+
+          if (scaleDiff < app.canvas.ds.min_scale)
+            scaleDiff = app.canvas.ds.min_scale
+          else if (scaleDiff > app.canvas.ds.max_scale)
+            scaleDiff = app.canvas.ds.max_scale
+
+          app.canvas.ds.scale = scaleDiff
+
+          // Code from LiteGraph
           if (Math.abs(app.canvas.ds.scale - 1) < 0.01) {
             app.canvas.ds.scale = 1
           }
 
           const newScale = app.canvas.ds.scale
 
-          var oldCenter = convertCanvasToOffset(
-            [center.clientX, center.clientY],
-            oldScale
-          )
-          var newCenter = convertCanvasToOffset(
-            [center.clientX, center.clientY],
-            newScale
-          )
+          const convertScaleToOffset = scale => [
+            center.clientX / scale - app.canvas.ds.offset[0],
+            center.clientY / scale - app.canvas.ds.offset[1]
+          ]
+          var oldCenter = convertScaleToOffset(oldScale)
+          var newCenter = convertScaleToOffset(newScale)
 
           app.canvas.ds.offset[0] += newX + newCenter[0] - oldCenter[0]
           app.canvas.ds.offset[1] += newY + newCenter[1] - oldCenter[1]
@@ -150,14 +138,14 @@ app.registerExtension({
   }
 });
 const processMouseDown = LGraphCanvas.prototype.processMouseDown;
-LGraphCanvas.prototype.processMouseDown = function(e) {
+LGraphCanvas.prototype.processMouseDown = function (e) {
   if (touchZooming || touchCount) {
     return;
   }
   return processMouseDown.apply(this, arguments);
 };
 const processMouseMove = LGraphCanvas.prototype.processMouseMove;
-LGraphCanvas.prototype.processMouseMove = function(e) {
+LGraphCanvas.prototype.processMouseMove = function (e) {
   if (touchZooming || touchCount > 1) {
     return;
   }
